4f39c9fe39c5940cdbda770955aa3f185614a69c
Adam Stefek <D21735@oaopava.cz>
2024-06-09 19:15:52 +0200

    9.6

--- test.py
+++
@@ -1,9 +0,0 @@
-print("Program pro výpočet kvádru a jeho obsahu")
-
-a = float(input("Zadej stranu a: "))
-b = float(input("Zadej stranu b: "))
-c = float(input("Zadej stranu c: "))
-
-p = 2 *(a*b + b*c + c*a)
-v = a*b*c
-print(f"Zadaný kvádr má povrch {p} a objem {v}")
